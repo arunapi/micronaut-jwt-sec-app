08eb2c921788d1b7982ad1aa4ddace6bab9090f3
Arun Abraham <arunapi@gmail.com>
2020-09-16 14:37:28 -0400

    Created app using cli

---
+++ README.md
@@ -0,0 +1,12 @@
+# Sample micronaut application to demo JWT auth
+
+
+
+## Feature http-client documentation
+
+- [Micronaut Micronaut HTTP Client documentation](https://docs.micronaut.io/latest/guide/index.html#httpClient)
+
+## Feature security-jwt documentation
+
+- [Micronaut Micronaut Security JWT documentation](https://micronaut-projects.github.io/micronaut-security/latest/guide/index.html)
+
---
+++ src/test/java/micronaut/jwt/sec/app/MicronautJwtSecAppTest.java
@@ -0,0 +1,21 @@
+package micronaut.jwt.sec.app;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class MicronautJwtSecAppTest {
+
+    @Inject
+    EmbeddedApplication application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
